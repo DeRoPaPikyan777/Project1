d2982b8a2ff7c01b7bf6bc5ffa137492e998cd0f
DeRoPaPikyan777 <dpapikyan7@gmail.com>
2021-09-12 00:21:37 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const random = require('random');
+
+const app = express();
+
+
+app.use(express.static(`public`))
+
+app.set(`view engine`, `ejs`);
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/' , (req , res)=>{
+
+   res.render(`index`);
+
+})
+
+app.get('/Info' , (req , res)=>{
+        
+    data = {
+        name:"Derenik",
+        email:"dpapikyan@gmail.com",
+        languaages:['c#','javascript','php']
+    }
+    res.render(`info`, {data:data})
+
+})
+
+app.get('/random' , (req , res)=>{
+    let rand = "";
+    if(req.query.max && req.query.min) {
+        rand = random.int(+req.query.min, +req.query.max)
+    }
+    res.render(`random`, {rand: rand})
+
+})
+
+app.get('/contact' , (req , res)=>{
+
+   res.render('contact')
+
+})
+
+app.post('/contact' , (req , res)=>{
+    let info = req.body;
+    console.log(info);
+    res.render(`contact-success`, {info})
+
+})
+
+
+
+app.listen(3000)
